3c730eb42b2b8480910c806309a4ec3a71e3619a
Timam <mail@timam.io>
2023-11-24 08:14:56 +0600

    init: argocd up

---
+++ sandbox/argo.tf
@@ -0,0 +1,36 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+}
+
+resource "helm_release" "argo-rollouts" {
+  name             = "argo-rollouts"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-rollouts"
+  namespace        = "argocd"
+  create_namespace = true
+  values = [
+    <<-EOT
+dashboard:
+  enabled: true
+  service:
+    type: ClusterIP
+EOT
+  ]
+}
+
+output "argocd-admin" {
+  value = <<EOF
+##run the following command to get argocd initial admin password##
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+##run the following command to access argocd
+minikube service argocd-server -n argocd
+
+##run the following command to access argo rollouts##
+minikube service argo-rollouts-dashboard -n argocd
+EOF
+}
